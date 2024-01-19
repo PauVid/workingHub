02c6712f67ecabdd612e5b5ebbaacab3f7f11238
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-01-19 11:36:36 +0100

    Ejercicio 1

--- KATAS JS DOM/Ejercicio 1/index.js
+++ KATAS JS DOM/Ejercicio 1/index.js
@@ -5,3 +5,18 @@
 // 5. Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe"
 // 6. Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe"
 
+const show = document.querySelector(".showme");
+console.log(show);
+
+const pillado = document.querySelector("h1#pillado");
+console.log(pillado);
+
+console.log(document.querySelectorAll("p"));
+
+console.log(document.querySelectorAll(".pokemon"));
+
+console.log(document.querySelectorAll("[data-function='testMe']"));
+
+console.log(document.querySelectorAll(3,"[data-function='testMe']"));
+
+
