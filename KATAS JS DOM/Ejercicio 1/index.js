ba1735d72bcd4724ee665fa7c445b9bbd6ae73df
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-01-19 08:52:02 +0100

    Ej.1

---
+++ KATAS JS DOM/Ejercicio 1/index.js
@@ -0,0 +1,7 @@
+// 1. Usa querySelector para mostrar por consola el botón con la clase .showme
+// 2. Usa querySelector para mostrar por consola el h1 con el id #pillado
+// 3. Usa querySelector para mostrar por consola todos los p
+// 4. Usa querySelector para mostrar por consola todos los elementos con la clase .pokemon
+// 5. Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe"
+// 6. Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe"
+
