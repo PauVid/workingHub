f46af005b19bffdbe8db8baa2ac6d1645d32266b
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-01-29 20:10:46 +0100

    projecte_pinterest

--- ProyectoAsync/Pinterest/main.js
+++ ProyectoAsync/Pinterest/main.js
@@ -1,2 +1,12 @@
 import './style.css'
 
+const imagesPint = () => {
+    const imageContainer = document.createElement("div");
+    const container = document.querySelector("#app");
+
+    container.append(imageContainer);
+
+    console.log("New Div!");
+
+};
+
