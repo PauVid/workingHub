0bd48cc3cafcc331e06566a87dba77681a7563e2
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-01-29 15:22:41 +0100

    saving

---
+++ NODEjs/Node_Scripting/main.js
@@ -0,0 +1,20 @@
+// instalamos fs escribiendo en el terminal: npm i fs
+
+const fs = require("fs");
+
+
+// fs.writeFile("nuevoArchivo.txt", "Esto es lo primero que he escrito en mi archivo", (err, data) => {
+//     console.log("Se ha escrito correctamente");
+
+//     fs.readFile("nuevoArchivo.txt", "utf-8", (error, data) => {
+//         console.log(data);
+//     });
+// });
+
+
+const arrayCoches = [
+    {
+        marca: "Peugeot",
+        precio: 20034
+    },
+];
